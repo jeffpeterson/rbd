88ad873074be83ceb0d7d912b02d5cdc6716860f
Jeff Peterson <jeff@yak.sh>
2021-01-10 20:36:36 -0800

    Start adding cli

--- lib/rbd.rb
+++ lib/rbd.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-require_relative "rbd/version"
+require_relative 'rbd/version'
+require_relative 'rbd/cli'
 
 module Rbd
   class Error < StandardError; end
---
+++ lib/rbd/cli.rb
@@ -0,0 +1,47 @@
+require 'optparse'
+require 'ostruct'
+
+module Rbd
+  class NoMainError < StandardError; end
+end
+
+class Rbd::Cli
+  attr_reader :cmd, :flags, :args
+
+  def initialize
+    @flags = OpenStruct.new
+    @args = ARGV.clone
+    opt_parser.parse!(@args, into: flags)
+    @cmd = @args.shift || 'run'
+  end
+
+  def run!
+    return puts 'Todo: help' if flags.help
+
+    case cmd
+    when 'run'
+
+      raise Rbd::NoMainError unless load! 'main', 'lib/main'
+    else
+      puts "Not a valid command: #{cmd}"
+    end
+  rescue Rbd::NoMainError
+    puts 'Could not run project. Expected main.rb file'
+  end
+
+  def opt_parser
+    @opt_parser ||= OptionParser.new do |op|
+      op.on('-h', '--help', 'Displays help')
+    end
+  end
+
+  def load!(*files)
+    file, *files = files
+    return if file.nil?
+
+    load "#{Dir.pwd}/#{file}.rb"
+    file
+  rescue LoadError => e
+    load!(*files)
+  end
+end
